2c7eba4ba8348f41715a1863506a0b378a4b5e65
Bohdan Shubenok <bohdan.shubenok@sigma.software>
2023-01-16 18:49:33 +0200

    DONT give a fuck for now

--- src/utilities/nimble_npl_os.rs
+++ src/utilities/nimble_npl_os.rs
@@ -10,7 +10,8 @@ pub fn ble_npl_hw_enter_critical() {
 
   #[cfg(not(esp32c3))]
   unsafe {
-    xPortEnterCriticalTimeout(&mut ble_port_mutex, portMUX_NO_TIMEOUT)
+    _ = npl_freertos_hw_enter_critical();
+    // xPortEnterCriticalTimeout(&mut ble_port_mutex, portMUX_NO_TIMEOUT)
   };
 }
 
@@ -24,6 +25,7 @@ pub fn ble_npl_hw_exit_critical() {
 
   #[cfg(not(esp32c3))]
   unsafe {
-    vPortExitCritical(&mut ble_port_mutex)
+    _ = npl_freertos_hw_exit_critical(0);
+    // vPortExitCritical(&mut ble_port_mutex)
   };
 }
